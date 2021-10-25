9260c8ee5d042fc5aa8309ecf130828612b2ff31
Iovan Barros <iovanbarros@outlook.com>
2021-10-25 10:20:24 -0300

    adding encapsulation

--- SecurityCodeScanner/Domain/ScannerLog.cs
+++ SecurityCodeScanner/Domain/ScannerLog.cs
@@ -2,9 +2,9 @@
 {
     public class ScannerLog
     {
-        public string LogType { get;  set; }
-        public string File { get;  set; }
-        public int Line { get;  set; }
+        public string LogType { get;  private set; }
+        public string File { get; private set; }
+        public int Line { get; private set; }
 
         public ScannerLog(string logType, string file, int lineNumber)
         {
