0b9d7b2460eb7ec17b8a0780ef11820b977ccec9
Iovan Barros <iovanbarros@outlook.com>
2021-10-26 22:43:57 -0300

    camel case

--- SecurityCodeScanner/Application/Service/SecurityCheck.cs
+++ SecurityCodeScanner/Application/Service/SecurityCheck.cs
@@ -6,38 +6,38 @@ namespace SecurityCodeScanner.Application.Service
 {
     public static class SecurityCheck
     {
-        public static void SecurityCheckSteps(ScannerRequest scannerRequest, string input, string file, int linenumber)
+        public static void SecurityCheckSteps(ScannerRequest scannerRequest, string input, string file, int lineNumber)
         {
-            FindSensitiveData(scannerRequest, input, file, linenumber);
+            FindSensitiveData(scannerRequest, input, file, lineNumber);
 
-            FindSQLInjection(scannerRequest, input, file, linenumber);
+            FindSQLInjection(scannerRequest, input, file, lineNumber);
 
-            FindSiteScripting(scannerRequest, input, file, linenumber);
+            FindSiteScripting(scannerRequest, input, file, lineNumber);
 
         }
 
-        public static void FindSensitiveData(ScannerRequest scannerRequest, string input, string file, int linenumber)
+        public static void FindSensitiveData(ScannerRequest scannerRequest, string input, string file, int lineNumber)
         {
             string[] findSensitiveData = new string[] { "Checkmarx", "Hellman & Friedman", "$1.15b" };
 
             if (findSensitiveData.All(input.Contains))
-                scannerRequest.AddLog("Sensitive Data Exposure", file, linenumber);
+                scannerRequest.AddLog("Sensitive Data Exposure", file, lineNumber);
         }
 
-        public static void FindSQLInjection(ScannerRequest scannerRequest, string input, string file, int linenumber)
+        public static void FindSQLInjection(ScannerRequest scannerRequest, string input, string file, int lineNumber)
         {
             var pattern = "^\"(SELECT|Select|select)\\b.*\\b(WHERE|Where|where)\\b\\s.*(%s)\"$";
 
             if (Regex.IsMatch(input, pattern))
-                scannerRequest.AddLog("SQL Injection", file, linenumber);
+                scannerRequest.AddLog("SQL Injection", file, lineNumber);
         }
 
-        public static void FindSiteScripting(ScannerRequest scannerRequest, string input, string file, int linenumber)
+        public static void FindSiteScripting(ScannerRequest scannerRequest, string input, string file, int lineNumber)
         {
             string[] crossSiteScripting = new string[] { "Alert()" };
 
             if (crossSiteScripting.All(input.Contains))
-                scannerRequest.AddLog("Cross Site Scripting", file, linenumber);
+                scannerRequest.AddLog("Cross Site Scripting", file, lineNumber);
         }
     }
 }
